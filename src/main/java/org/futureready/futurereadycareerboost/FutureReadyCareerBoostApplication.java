b7af436093b44fcaef3a9a35ce9a3428c6d6a929
VitoriGjipali <vitorigjipali15@gmail.com>
2025-05-17 10:56:42 +0200

    first commit

---
+++ src/main/java/org/futureready/futurereadycareerboost/FutureReadyCareerBoostApplication.java
@@ -0,0 +1,13 @@
+package org.futureready.futurereadycareerboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureReadyCareerBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureReadyCareerBoostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/futureready/futurereadycareerboost/FutureReadyCareerBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.futureready.futurereadycareerboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureReadyCareerBoostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
